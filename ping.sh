bd8914cae9fb5f6619317f046be5ac1aeadc29f0
soloaplicaciones <info@soloaplicaciones.com>
2026-05-18 20:17:46 +0200

    x

--- ping.sh
+++ ping.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -i ~/.ssh/dev \
+ssh -i /c/Users/admin/.ssh/dev \
   -o StrictHostKeyChecking=no \
   dev@01.servidorpro.com \
-  "echo Conexion OK && ls /"
+  "echo OK && hostname"
